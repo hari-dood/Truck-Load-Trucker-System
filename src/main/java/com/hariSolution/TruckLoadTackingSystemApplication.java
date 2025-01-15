216473182906ec656ee7a566cf18ff1c042eed51
hariharan-anbu <anbuhari200@gmail.com>
2025-01-15 06:57:27 +0530

    the name of the project has been changed as track-load-management-system

--- src/main/java/com/hariSolution/TruckLoadTackingSystemApplication.java
+++ src/main/java/com/hariSolution/TruckLoadTackingSystemApplication.java
@@ -3,14 +3,13 @@ package com.hariSolution;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableCaching
-public class TruckLoadManagementSystemApplication {
+public class TruckLoadTackingSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TruckLoadManagementSystemApplication.class, args);
+		SpringApplication.run(TruckLoadTackingSystemApplication.class, args);
 	}
 
 }
--- src/test/java/com/hariSolution/TruckLoadTackingSystemApplicationTests.java
+++ src/test/java/com/hariSolution/TruckLoadTackingSystemApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TruckLoadManagementSystemApplicationTests {
+class TruckLoadTackingSystemApplicationTests {
 
 	@Test
 	void contextLoads() {
